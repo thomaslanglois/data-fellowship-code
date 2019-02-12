d11ca8351157f817075829847622b18033965de1
Thomas Langlois <thomas.langlois@mac.com>
2019-02-12 22:52:16 +0000

    final push

--- modules/introduction_to_python/challenges/challenge1.py
+++ modules/introduction_to_python/challenges/challenge1.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-    pass
+    x = 0
+    while x == 0:
+        x, y = list(map(int, input('Enter two whole numbers separated by a space: ').split()))
+        if x == 0:
+            print('The first number has to be greater than 0!')
+    print('{} + {} = {}'.format(x, y, x + y))
+    print('{} - {} = {}'.format(x, y, x - y))
+    print('{} * {} = {}'.format(x, y, x * y))
+    print('{} / {} = {}'.format(y, x, y / x))
+

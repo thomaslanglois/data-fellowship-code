13b43dca3e09d8583d06dc1e1452b7ce83156879
Thomas Langlois <thomas.langlois@mac.com>
2019-02-12 23:13:32 +0000

    final push

--- modules/introduction_to_python/challenges/challenge2.py
+++ modules/introduction_to_python/challenges/challenge2.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
         if len(s) != 3:
             print('Sorry I really need 3 strings from you, not {}!'.format(len(s)))
     lengths = [len(l) for l in s]
-    print(lengths)
+    print('lengths: {}'.format(lengths))
     for length, first3, last3 in zip(lengths,[l[:3] for l in s], [l[-3:] for l in s]):
         print('len: {}, first 3: {}, last 3: {}'.format(length, first3, last3))
